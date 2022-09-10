31d242bb76e7d7a6794f4702bc26aa984f5a5f0b
Johnny Backlund <johnnybacklund@hotmail.com>
2022-09-10 13:46:13 +0200

    Changed paragraphs in main branch

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,22 @@ export default function Home() {
         <h3 className="heading-sm">Heading 2</h3>
         <span className="label">Label</span>
         <p className="body">
+          Nulla pharetra egestas vulputate. Aenean eget imperdiet lorem. Duis
+          bibendum libero eu felis auctor, a faucibus ipsum pharetra. Vivamus
+          vestibulum, arcu dignissim tristique porttitor, lectus neque
+          pellentesque sem, feugiat venenatis metus sem sed augue.
+        </p>
+        <p className="body">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          posuere dolor ac accumsan consectetur. Quisque eleifend libero odio.
+          Sed posuere congue lacinia. Nulla sollicitudin, est id ullamcorper
+          tempor, tortor erat suscipit ante, id accumsan elit est ut dolor.
+          Nulla pharetra egestas vulputate. Aenean eget imperdiet lorem. Duis
+          bibendum libero eu felis auctor, a faucibus ipsum pharetra. Vivamus
+          vestibulum, arcu dignissim tristique porttitor, lectus neque
+          pellentesque sem, feugiat venenatis metus sem sed augue.
+        </p>
+        <p className="body">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
           posuere dolor ac accumsan consectetur. Quisque eleifend libero odio.
           Sed posuere congue lacinia. Nulla sollicitudin, est id ullamcorper
