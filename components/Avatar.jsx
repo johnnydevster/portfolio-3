49e2fe85af75fc071900ed09ee172c07227a8671
Johnny Backlund <johnnybacklund@hotmail.com>
2022-09-15 21:56:48 +0200

    Testing different hero setups

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -8,13 +8,6 @@ export default function Avatar() {
       <div className="relative z-[-10] rounded-full h-80 w-80 p-4 bg-slate-dark-lighter">
         <Image src={AvatarImage} className="rounded-full" />
       </div>
-      <div className="flex flex-col gap-2 justify-center bg-slate-dark-lighter p-11 rounded-r-xl relative -left-24 pl-28 z-[-20]">
-        <h1 className="heading text-slate-100">Johnny</h1>
-        <hr className="border-slate-800" />
-        <h2 className="label uppercase text-base text-slate-300">
-          front end web developer
-        </h2>
-      </div>
     </>
   );
 }
--- components/modules/HeroModule.jsx
+++ components/modules/HeroModule.jsx
@@ -1,16 +1,30 @@
 import PlanetOne from "../../public/assets/svgs/PlanetOne";
 import Star from "../../public/assets/svgs/Star";
 import Avatar from "../Avatar";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 export default function HeroModule() {
   return (
-    <section className="h-[calc(100vh-var(--header-height-minimized))] flex w-full justify-center items-center relative overflow-hidden">
+    <section className="h-[calc(100vh-var(--header-height-minimized))] relative overflow-hidden grid grid-cols-2 place-items-center">
       <div className="flex flex-col gap-2">
-        <h1 className="heading-lg">Johnny Backlund</h1>
-        <h2 className="label">Front End Web Developer</h2>
+        <span className="label text-slate-300">👋 hey there, I'm</span>
+        <h1 className="heading-lg text-slate-100">Johnny Backlund</h1>
+        <h2 className="label text-sky-200 text-2xl">
+          <span>
+            <KeyboardArrowLeftIcon />
+          </span>
+          Front End Web Developer
+          <span>
+            <KeyboardArrowRightIcon />
+          </span>
+        </h2>
       </div>
-      <PlanetOne />
-      <Star />
+
+      <Avatar />
+
+      {/*  <PlanetOne />
+      <Star /> */}
     </section>
   );
 }
